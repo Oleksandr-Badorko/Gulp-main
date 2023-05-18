61d857eed0f19b46799be524841ec08416a54996
Badorko Oleksandr <sachabv1999@ukr.net>
2023-05-18 19:54:15 +0300

    my-gulp-configurations

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("styles", function () {
+  return (
+    gulp
+      .src("./scss/styles.scss")
+      .pipe(sourcemaps.init())
+      .pipe(sass().on("error", sass.logError))
+      .pipe(cleanCSS({ compatibility: "ie8" }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest("./css"))
+      .pipe(browserSync.stream())
+  );
+});
+
+gulp.task("watch", function () {
+  browserSync.init({
+    server: "./",
+  });
+
+  gulp.watch("./scss/**/*.scss", gulp.series("styles"));
+  gulp.watch(["./*.html", "./js/**/*.js"]).on("change", browserSync.reload);
+});
+
+gulp.task("default", gulp.series("styles", "watch"));
